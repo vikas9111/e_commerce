a6abcbd76b817500dc41caa4028c4eeb3d564fd5
vikas9111 <vikaspandey1099@gmail.com>
2023-08-07 18:09:47 +0530

    added jquery and bootstrap by esbuild

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
